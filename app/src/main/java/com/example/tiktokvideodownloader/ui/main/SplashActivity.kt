3b874d09d3d94e90b4d15e2d5c1d7581fad505bc
HP <abdullahlatif9123@gmail.com>
2022-12-15 21:54:11 +0500

    Projectt

--- app/src/main/java/com/example/tiktokvideodownloader/ui/main/SplashActivity.kt
+++ app/src/main/java/com/example/tiktokvideodownloader/ui/main/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -50,7 +51,7 @@ class SplashActivity : AppCompatActivity() {
                 progressBar!!.visibility =View.INVISIBLE
             }
         }.start()
-        Handler().postDelayed({
+        Handler(Looper.myLooper()!!).postDelayed({
             text!!.visibility = View.VISIBLE
         }, 5100)
         text!!.setOnClickListener{
